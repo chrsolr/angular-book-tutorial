3041ea29006266280a8cac1f870de04b908cfebd
Christian Soler <christiansoler@Christians-MacBook-Pro.local>
2019-11-30 16:52:54 -0600

    updated

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -2,7 +2,7 @@ export class ProductService {
   getProducts(): object[] {
     return [
       {
-        imageUrl: 'http://loremflickr.com/150/150?random=1',
+        imageUrl: 'http://loremflickr.com/500/500?random=1',
         productName: 'Product 1',
         releasedDate: new Date('May 31, 2019'),
         description: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
@@ -11,7 +11,7 @@ export class ProductService {
         numberOfReviews: 2,
       },
       {
-        imageUrl: 'http://loremflickr.com/150/150?random=2',
+        imageUrl: 'http://loremflickr.com/500/500?random=2',
         productName: 'Product 2',
         releasedDate: new Date('March 21, 2018'),
         description: 'Lorem, ipsum dolor sit amet consectetur.',
@@ -19,12 +19,20 @@ export class ProductService {
         numberOfReviews: 20,
       },
       {
-        imageUrl: 'http://loremflickr.com/150/150?random=3',
+        imageUrl: 'http://loremflickr.com/500/500?random=3',
         productName: 'Product 3',
         releasedDate: new Date('June 1, 2011'),
         description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur velit corrupti.',
         rating: 4,
         numberOfReviews: 2000,
+      },
+      {
+        imageUrl: 'http://loremflickr.com/500/500?random=4',
+        productName: 'Product 4',
+        releasedDate: new Date('December 10, 2001'),
+        description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit.',
+        rating: 3,
+        numberOfReviews: 500,
       },
     ]
   }
