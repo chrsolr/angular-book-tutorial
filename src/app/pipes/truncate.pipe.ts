8934e7415acd6a2233f940d2a339af365351b216
Christian Soler <chr.solr@gmail.com>
2019-12-03 16:26:30 -0600

    Updated

--- src/app/pipes/truncate.pipe.ts
+++ src/app/pipes/truncate.pipe.ts
@@ -3,6 +3,6 @@ import { Pipe, PipeTransform } from '@angular/core'
 @Pipe({ name: 'truncate' })
 export class TruncatePipe implements PipeTransform {
   transform(value: string, limit: number) {
-    return `${value.substring(0, limit)}...`
+    return value.length >= limit ? `${value.substring(0, limit)}...` : value
   }
 }
